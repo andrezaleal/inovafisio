06079ffed0eabda6f4b75a0f6da9db12a102bba6
Andreza Leal <andrezaleal@alu.ufc.br>
2021-03-21 14:54:55 -0300

    botao prosseguir

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@ $(document).ready(function(){
   // TABS
   $('ul.tabs').tabs();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
